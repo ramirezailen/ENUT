26a679fb6a2758fcb3e4a23e451a4daac6ad9ddf
ramirezailen <ramirezailenn@gmail.com>
2023-01-23 15:27:46 -0300

    final análisis indicador ENUT de base miembro + excel con código variables

--- script_miembros.R
+++ script_miembros.R
@@ -13,45 +13,29 @@ base_persona <- read.table("C:/Users/ailen/Documents/GitHub/ENUT/enut2021_base.t
 )
 
 
-#Hogares con presencia de niñxs de hasta 2 años  que asisten al jardín y que además reciben asistencia en cuidados (gestión estatal, privada o comunitaria)
+#Hogares con presencia de niñxs de hasta 5 años  que asisten al jardín y que además reciben asistencia en cuidados (gestión estatal, privada o comunitaria)
 
-hogares_dos_años_jardin <- base_miembro %>%
+menores_5_años <-base_miembros %>% 
+  filter( BHCH03 >= 5 &  BHCH08 == 1) %>% 
   group_by (ID) %>%
   summarise (
-    n = length (N_MIEMBRO [BHCH09 == 1 & BHCH11 >= 2 & BHCH08 == 1])
-    )
+    cinco_años_asistencia_no_excluyente = length(N_MIEMBRO[BHDC01_03 == 1 | BHDC01_04 == 1 | BHDC01_06 == 1]),
+    cinco_años_comunitario = length(N_MIEMBRO[BHDC01_03 == 1 &  BHDC01_04 != 1 & BHDC01_06 != 1]),
+    cinco_años_privado = length(N_MIEMBRO[BHDC01_03 != 1 &  BHDC01_04 != 1 & BHDC01_06 == 1]),
+    cinco_años_estatal = length(N_MIEMBRO[BHDC01_03 != 1 &  BHDC01_04 == 1 & BHDC01_06 != 1]),
+    cinco_años_total = length(N_MIEMBRO)
+  )
 
 
- # merge ambas tablas    
-persona_miembro <- merge(
-  x = base_miembro,
-  y = base_persona,
-  by = "ID"
-)
+#ponderador
 
- #ponderar n 
-  
-hogares_ponderado_2_años <- hogares_dos_años_jardin %>%
-  select(ID, n, WHOG) %>% 
-  mutate(n = ifelse(n == 0, "Sin presencia de ninxs", "Con presencia de ninxs que asisten y reciben asistencia estatal")) %>% 
-  group_by(n) %>% 
-  summarise(
-    hogares = sum(WHOG)
-    ) 
+persona_miembro <- menores_5_años%>%
+  left_join( ., base_usuario, by ='ID')%>%
+  select(cinco_años_asistencia_no_excluyente,cinco_años_comunitario, cinco_años_privado,cinco_años_estatal, cinco_años_total, WHOG) 
 
 
-#Hogares con presencia de niñxs de hasta 3 años  que asisten al jardín y que además reciben asistencia en cuidados (gestión estatal, privada o comunitaria)
-
- 
-
-
-# Hogares con presencia de niñxs de hasta 5 años  que asisten al jardín y que además reciben asistencia en cuidados (gestión estatal, privada o comunitaria)
-
-1 o más niños que vayan a estblecimiento y sea gestión estatal, privada o comunitaria
-
-id hogar agrupar x id (group by) y contar la cantidad de personas
-
-cuenta cantidad de filas de n miembros {condición edad mayor a dos, demadnante de cuidados(1 demandante de cuidado-ver), y tipo de establecimiento} tabla ID hogar y variable N unidad análisis hogar. 
-
-
-JOIN con base  usuario por id y hacer ponderar n y hacer porcentaje y absoluto. 
+#porcentajes
+porcentajes <-  persona_miembro %>% 
+  summarise(cinco_años_asistencia_no_excluyente = sum(WHOG[cinco_años_asistencia_no_excluyente >= 1]),
+            cinco_años_total = sum(WHOG), 
+            porcentaje = cinco_años_asistencia_no_excluyente/cinco_años_total * 100 )
